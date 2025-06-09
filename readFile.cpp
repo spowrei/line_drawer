805ed93bbf82727fa005e11823ac412fbf48cbcb
Mustafa Kaplan <mkaplan2005@gmail.com>
2025-06-09 14:34:28 +0300

    files created

---
+++ readFile.cpp
@@ -0,0 +1,4 @@
+// dosydaki sayıları matrise aktarıp return et
+// matrisi harita boyutuna göre oluştur
+
+// map boyutunu da return edebilirsin
---
+++ readFile.hpp
@@ -0,0 +1,4 @@
+#ifndef READ_FILE_HPP
+#define READ_FILE_HPP
+
+#endif
