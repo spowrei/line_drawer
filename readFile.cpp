b17cd5e12f16f3d11e7bf648ea9d2145cef9612d
Mustafa Kaplan <mkaplan2005@gmail.com>
2025-06-11 19:55:52 +0300

    the data will now be retrieved from the map.map file

--- readFile.cpp
+++ readFile.cpp
@@ -1,4 +1,45 @@
-// dosydaki sayýlarý matrise aktarýp return et
-// matrisi harita boyutuna göre oluþtur
+#include "readFile.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-// map boyutunu da return edebilirsin
+int read_file(std::vector<int> &map)
+{
+    int map_size = 0;
+    std::ifstream file("map.map");
+    if (!file.is_open())
+    {
+        std::cout << "Failed to open file." << std::endl;
+        return (-1);
+    }
+
+    std::string first_line;
+    std::getline(file, first_line);
+
+    std::istringstream iss(first_line);
+    std::vector<int> first_row;
+    int value;
+
+    while (iss >> value)
+    {
+        first_row.push_back(value);
+    }
+
+    map_size = first_row.size();
+    map.reserve(map_size * map_size); 
+    map.insert(map.begin(), first_row.begin(), first_row.end());
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream line_stream(line);
+        while (line_stream >> value)
+        {
+            map.push_back(value);
+        }
+    }
+
+    file.close();
+    return map_size;
+}
--- readFile.hpp
+++ readFile.hpp
@@ -1,4 +1,8 @@
 #ifndef READ_FILE_HPP
 #define READ_FILE_HPP
 
+#include <vector>
+
+	int read_file(std::vector<int>& map);
+
 #endif
